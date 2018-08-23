03fa60ad97ea5063f92fff17e82c8e4b61f06852
Remi Geny-Dumont <remi.genydumont@gmail.com>
2018-08-23 15:48:26 +0200

    update architecture

---
+++ src/test/java/fr/pizzeria/dao/PizzaMemDaoTest.java
@@ -0,0 +1,14 @@
+package fr.pizzeria.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PizzaMemDaoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
